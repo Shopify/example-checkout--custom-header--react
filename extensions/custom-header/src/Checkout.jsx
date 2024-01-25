616e2b0f9178e1d3482b9c02626267c5572d1cb4
Greg Craft <greg.craft@shopify.com>
2024-01-25 17:31:50 -0600

    Configuring header extension

--- extensions/custom-header/src/Checkout.jsx
+++ extensions/custom-header/src/Checkout.jsx
@@ -1,22 +1,36 @@
 import {
-  Banner,
-  useApi,
-  useTranslate,
+  Image,
+  InlineLayout,
   reactExtension,
 } from '@shopify/ui-extensions-react/checkout';
 
 export default reactExtension(
-  'purchase.checkout.block.render',
+  'purchase.checkout.header.render-after',
   () => <Extension />,
 );
 
-function Extension() {
-  const translate = useTranslate();
-  const { extension } = useApi();
+// The image URLs to display in the header; replace these with your own images. Learn more:
+// https://help.shopify.com/en/manual/shopify-admin/productivity-tools/file-uploads
+const images = [
+  "https://cdn.shopify.com/path/to/image/file_name_1.png",
+  "https://cdn.shopify.com/path/to/image/file_name_2.png",
+  "https://cdn.shopify.com/path/to/image/file_name_3.png",
+  "https://cdn.shopify.com/path/to/image/file_name_4.png",
+  "https://cdn.shopify.com/path/to/image/file_name_5.png",
+  "https://cdn.shopify.com/path/to/image/file_name_6.png",
+  "https://cdn.shopify.com/path/to/image/file_name_7.png",
+  "https://cdn.shopify.com/path/to/image/file_name_8.png",
+];
 
+function Extension() {
   return (
-    <Banner title="custom-header">
-      {translate('welcome', {target: extension.target})}
-    </Banner>
+      <InlineLayout
+        columns="1fr"
+        spacing="loose"
+      >
+        {images.map((source, index) => (
+          <Image key={index} source={source} aspectRatio={1} />
+        ))}
+      </InlineLayout>
   );
 }
