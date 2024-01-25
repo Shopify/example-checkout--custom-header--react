474b4568d915e10fdb07789637b174e27f96bb3f
Greg Craft <greg.craft@shopify.com>
2024-01-25 17:51:21 -0600

    Add header for thank you target

--- extensions/custom-header/src/ThankYou.jsx
+++ extensions/custom-header/src/ThankYou.jsx
@@ -1,6 +1,6 @@
 import {reactExtension} from '@shopify/ui-extensions-react/checkout';
 import Extension from './Extension.jsx';
 
-export default reactExtension('purchase.checkout.header.render-after', () => (
+export default reactExtension('purchase.thank-you.header.render-after', () => (
   <Extension />
 ));
