49c638604fbed6ce173bff0d3ff2978521a64c15
Greg Craft <greg.craft@shopify.com>
2024-01-26 12:50:01 -0600

    Using a single image for the header image

--- extensions/custom-header/src/Extension.jsx
+++ extensions/custom-header/src/Extension.jsx
@@ -1,24 +1,9 @@
-import {Image, InlineLayout} from '@shopify/ui-extensions-react/checkout';
-
-// The image URLs to display in the header; replace these with your own images. Learn more:
-// https://help.shopify.com/en/manual/shopify-admin/productivity-tools/file-uploads
-const images = [
-  'https://cdn.shopify.com/path/to/image/file_name_1.png',
-  'https://cdn.shopify.com/path/to/image/file_name_2.png',
-  'https://cdn.shopify.com/path/to/image/file_name_3.png',
-  'https://cdn.shopify.com/path/to/image/file_name_4.png',
-  'https://cdn.shopify.com/path/to/image/file_name_5.png',
-  'https://cdn.shopify.com/path/to/image/file_name_6.png',
-  'https://cdn.shopify.com/path/to/image/file_name_7.png',
-  'https://cdn.shopify.com/path/to/image/file_name_8.png',
-];
+import {Image} from '@shopify/ui-extensions-react/checkout';
 
 export default function Extension() {
   return (
-    <InlineLayout columns="1fr" spacing="loose">
-      {images.map((source, index) => (
-        <Image key={index} source={source} aspectRatio={1} />
-      ))}
-    </InlineLayout>
+    // Replace the source with your own image url. Learn more:
+    // https://help.shopify.com/en/manual/shopify-admin/productivity-tools/file-uploads
+    <Image source="https://cdn.shopify.com/path/to/image/file_name.png" />
   );
 }
