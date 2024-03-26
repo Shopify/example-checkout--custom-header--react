8c7851026fb6a3a3d4ee1872165c6ff95f84140b
Jun Kim <jun.kim@shopify.com>
2024-03-26 15:56:21 -0700

    remove Image and BlockStack

--- extensions/custom-header/src/Extension.jsx
+++ extensions/custom-header/src/Extension.jsx
@@ -1,6 +1,4 @@
 import {
-  BlockStack,
-  Image,
   InlineLayout,
   Link,
   Text,
@@ -23,36 +21,31 @@ export default function Extension() {
 
   // [START custom-header.render]
   return (
-    // Replace the image source with your own image url. Learn more:
-    // https://help.shopify.com/en/manual/shopify-admin/productivity-tools/file-uploads
-    <BlockStack spacing="tight">
-      <Image source="https://cdn.shopify.com/path/to/image/file_name.png" />
-      <InlineLayout accessibilityRole="orderedList" spacing="tight">
+    <InlineLayout accessibilityRole="orderedList" spacing="tight">
+      <View
+        accessibilityRole="listItem"
+        inlineAlignment="center"
+        border="base"
+        padding="base"
+        background="base">
+        <Link to={storefrontUrl}>{name}</Link>
+      </View>
+      {steps.map(({label, handle, to}, index) => (
         <View
           accessibilityRole="listItem"
           inlineAlignment="center"
+          key={handle}
           border="base"
           padding="base"
-          background="base">
-          <Link to={storefrontUrl}>{name}</Link>
+          background={activeStep?.handle === handle ? 'subdued' : 'base'}>
+          {activeStep?.handle === handle || index > activeStepIndex ? (
+            <Text>{label}</Text>
+          ) : (
+            <Link to={to}>{label}</Link>
+          )}
         </View>
-        {steps.map(({label, handle, to}, index) => (
-          <View
-            accessibilityRole="listItem"
-            inlineAlignment="center"
-            key={handle}
-            border="base"
-            padding="base"
-            background={activeStep?.handle === handle ? 'subdued' : 'base'}>
-            {activeStep?.handle === handle || index > activeStepIndex ? (
-              <Text>{label}</Text>
-            ) : (
-              <Link to={to}>{label}</Link>
-            )}
-          </View>
-        ))}
-      </InlineLayout>
-    </BlockStack>
+      ))}
+    </InlineLayout>
   );
   // [END custom-header.render]
 }
